87dfd13972792c5c83f0eb5ac9570f5178e18812
TahraouiMustapha <moustaphaoussama1818@gmail.com>
2026-01-29 18:33:39 +0100

    add a sign in page with its route

---
+++ src/pages/signin.jsx
@@ -0,0 +1,9 @@
+const SingIn = () => {
+    return (
+        <>
+            <h3>sign in page</h3>
+        </>
+    )
+}
+
+export default SingIn;
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,6 +2,7 @@ import App from '../App'
 import Home from '../pages/home'
 import About from '../pages/about'
 import SignUp from '../pages/signup'
+import SignIn from '../pages/signin'
 import ErrorPage from '../pages/errorElement'
 
 const routes = [
@@ -12,8 +13,8 @@ const routes = [
         children: [
             { index: true, element: <Home /> },
             { path: "about", element: <About /> },
+            { path: "signin", element: <SignIn /> },
             { path: "signup", element: <SignUp /> }
-            // still sign in page
         ]
     }
 ]
