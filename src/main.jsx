8a327fd3ae143f571cccd84d7939d396f49a714c
TahraouiMustapha <moustaphaoussama1818@gmail.com>
2026-01-29 09:57:31 +0100

    fix: fix version conflict of tailwindcss

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
 
-createRoot(document.getElementById('root')).render(
+import routes from "./routes/routes";
+import './index.css';
+
+
+const router = createBrowserRouter(routes)
+
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,12 @@
-/** @type {import('tailwindcss').Config} */
+
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        rob: ['Roboto', 'sans-serif']
-      }
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                rob: ['Roboto', 'sans-serif']
+            }
+        }
     },
-  },
-  plugins: [],
+    plugins: []
 }
