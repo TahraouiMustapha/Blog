5a3a2ecdbea85a23bdbb15ae2aa1500379bb0d88
TahraouiMustapha <moustaphaoussama1818@gmail.com>
2026-02-04 00:38:51 +0100

    feat(sign up): handle sign up submitting

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,7 +1,66 @@
+import { useState } from "react";
+
 const SingUp = () => {
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState('')
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true)
+
+        const form = new FormData(e.target)
+        const data = Object.fromEntries(form)
+
+        try {
+            const response = await fetch('http://localhost:3000/api/users', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+
+            if (!response.ok) {
+                throw new Error(`Http error! status: ${await response.status}`)
+            }
+
+            const result = await response.json()
+            console.log(result)
+
+        } catch (error) {
+            setError(error.message)
+            console.error(error)
+        } finally {
+            setLoading(false)
+        }
+
+
+    }
+
     return (
         <>
-            <h3>sign up page</h3>
+            <form onSubmit={handleSubmit} method="POST">
+                <input
+                    name="username"
+                    type="email"
+                    required
+                />
+
+                <input
+                    name="password"
+                    type="password"
+                    required
+                    minLength={8}
+                    maxLength={16}
+                />
+
+                {error && <p> {error}</p>}
+
+                <button disabled={loading}>
+                    {loading ? 'Submiting...' : 'Submit'}
+                </button>
+            </form>
         </>
     )
 }
