60d5c564e9d9a569142f344740b8a5cebd733174
TahraouiMustapha <moustaphaoussama1818@gmail.com>
2026-03-22 12:21:25 +0100

    fix: style animation delay of about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,7 +1,11 @@
 const About = () => {
     return (
         <>
-            <h3>about page</h3>
+            <h3 className="animate-fade-in">about page 1</h3>
+            <h3 className="animationDelaySd animate-fade-in">about page 2</h3>
+            <h3 className="animationDelayRd animate-fade-in">about page 2</h3>
+            <h3 className="animationDelayTh animate-fade-in">about page 2</h3>
+
         </>
     )
 }
