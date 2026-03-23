cbec92de5b7747f8f7e685f05d2ce1eb96737623
TahraouiMustapha <moustaphaoussama1818@gmail.com>
2026-03-23 12:46:14 +0100

    style: fix home page design

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -4,13 +4,13 @@ import LinkBtn from './linkBtn'
 
 const Welcome = () => {
     return (
-        <div className='m-14'>
-            <div className='flex flex-col items-center gap-6'>
-                <h2 className='text-4xl font-bold'>
+        <div className='m-14 xl:w-[40%] xl:flex xl:justify-center'>
+            <div className='flex flex-col items-center gap-6 xl:gap-5 xl:w-[60%]'>
+                <h2 className='text-4xl xl:text-5xl/12 font-bold'>
                     Welcome to <Logo />
                 </h2>
-                <p className='text-lg/7 text-center'>
-                    Hi there! My name is Dominik Augustyn, and this is my blog: written by an aspiring web developer, for other aspiring developers. Feel free to read all about my insights, experiences, tips and reflections!
+                <p className='text-lg/7 text-justify'>
+                    Hi there! My name is Tahraoui Mustapha, and this is my blog: written by an aspiring web developer, for other aspiring developers. Feel free to read all about my insights, experiences, tips and reflections!
                 </p>
                 <div className='flex items-center justify-center gap-8 text-lg'>
                     <PrimaryBtn to='/signup'>Sign Up Now !</PrimaryBtn>
@@ -43,7 +43,7 @@ const Welcome = () => {
 
 const Hero = () => {
     return (
-        <div className='flex flex-col h-screen'>
+        <div className='flex flex-col h-screen border-b border-brdClr xl:flex-row xl:h-[55vh]'>
             <Welcome />
             <div className='flex-1 AboutMeImg bg-cover bg-no-repeat bg-center'>
                 image
