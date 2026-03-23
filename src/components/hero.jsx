f0f6e567f587cb2303dd8785bcd216e26ae24529
TahraouiMustapha <moustaphaoussama1818@gmail.com>
2026-03-23 12:54:24 +0100

    add an image to the hero part

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -21,49 +21,13 @@ const Welcome = () => {
     )
 }
 
-// old version 
-// const Welcome = () => {
-//     return (
-//         <div className='flex-1 flex justify-center border-r border-brdClr'>
-//             <div className='w-[50%] pt-7 pb-8 flex flex-col justify-between'>
-//                 <h2 className='text-6xl/17 font-bold'>
-//                     Welcome to <Logo />
-//                 </h2>
-//                 <p className='text-xl/8'>
-//                     Hi there! My name is Dominik Augustyn, and this is my blog: written by an aspiring web developer, for other aspiring developers. Feel free to read all about my insights, experiences, tips and reflections!
-//                 </p>
-//                 <div className='flex justify-start items-center gap-12 text-lg'>
-//                     <PrimaryBtn to='/signup'>Sign Up Now !</PrimaryBtn>
-//                     <LinkBtn to='/about'>Read about me</LinkBtn>
-//                 </div>
-//             </div>
-//         </div>
-//     )
-// }
-
 const Hero = () => {
     return (
         <div className='flex flex-col h-screen border-b border-brdClr xl:flex-row xl:h-[55vh]'>
             <Welcome />
-            <div className='flex-1 AboutMeImg bg-cover bg-no-repeat bg-center'>
-                image
-            </div>
+            <div className='flex-1 bg-[url("./assets/bigImg.jpg")]  bg-cover bg-no-repeat bg-center border-l border-brdClr overflow-hidden' />
         </div>
     )
 }
-
-
-// old version 
-
-// const Hero = () => {
-//     return (
-//         <div className='h-[55vh] border-b border-brdClr flex'>
-//             <Welcome />
-//             <div className='w-[50%]'>
-//                 image
-//             </div>
-//         </div>
-//     )
-// }
 
 export default Hero;
