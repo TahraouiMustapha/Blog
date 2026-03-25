4bf5c2cefe2875e50b6b6255d2ebca5a639d1353
TahraouiMustapha <moustaphaoussama1818@gmail.com>
2026-03-25 01:03:48 +0100

    style: style menu with animation in opening

--- src/components/DropDownMenu.jsx
+++ src/components/DropDownMenu.jsx
@@ -1,18 +1,43 @@
-import { X } from "lucide-react"
+import { X, House, Info, LogIn } from "lucide-react"
+import { Logo } from "./Logo"
+import LinkBtn from "./linkBtn"
+import PrimaryBtn from "./primaryBtn"
 
 const DropDownMenu = ({ handleClick }) => {
     return (
-        <div className="bg-gray-500 absolute h-screen w-screen inset-0 flex ">
+        <div className="bg-gray-500 absolute h-screen w-screen inset-0 flex animate-open-overlay">
+            {/* backdrop */}
             <div
                 onClick={handleClick}
                 className="flex-1 relative" >
                 <X className="stroke-white stroke-2 absolute top-1 right-2" />
             </div>
-            <div className="bg-white h-full w-[45%]">
-                menu
+            {/* menu */}
+            <div className="bg-white h-full w-[45%] animate-open-menu flex flex-col p-4 gap-8">
+                <div>
+                    <Logo />
+                </div>
+
+                <div className="grid h-[33%] auto-rows-fr btnsMenu ">
+                    <div>
+                        <House />
+                        <LinkBtn to={'/'}>Home</LinkBtn>
+                    </div>
+                    <div>
+                        <Info />
+                        <LinkBtn to={'/about'}>About me</LinkBtn>
+                    </div>
+                    <div>
+                        <LogIn />
+                        <LinkBtn to={'/signin'}>Login</LinkBtn>
+                    </div>
+                    <div>
+                        <PrimaryBtn to={'/signup'}>Sign Up</PrimaryBtn>
+                    </div>
+                </div>
             </div>
         </div>
     )
 }
 
-export default DropDownMenu;
+export default DropDownMenu
