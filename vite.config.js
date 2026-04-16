fe6d2944860564fc410b574901e0b5b8922cffed
TahraouiMustapha <moustaphaoussama1818@gmail.com>
2026-04-16 10:36:25 +0100

    Revert "deploy: ready to deploy"
    This reverts commit 3a6540c835d83de32ebf2aab235aab491b673e03.

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://blog-api-rjm2.onrender.com',
+        target: 'http://localhost:3000',
         changeOrigin: true,
         secure: false, // Allow non-HTTPs connection
       }
